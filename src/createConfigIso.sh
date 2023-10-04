2f3466a08d9f9bad4cd6dcf83547b4c49980ca12
Torbjørn Bang <hei@torbjorn.dev>
2023-10-04 21:12:21 +0200

    Changed MIME boundary to avoid collisions with config text.

--- src/createConfigIso.sh
+++ src/createConfigIso.sh
@@ -10,16 +10,16 @@
 
 # Create minimal cloud init config to make router boot 
 cat << EOF > ciscosdwan_cloud_init.cfg
-Content-Type: multipart/mixed; boundary="===================================="
+Content-Type: multipart/mixed; boundary="==============u7fnxr6d=============="
 MIME-Version: 1.0
---====================================
+--==============u7fnxr6d==============
 #cloud-config
 vinitparam:
  - uuid : C8K-00000000-0000-0000-0000-000000000000
  - otp : 00000000000000000000000000000000
  - vbond : 0.0.0.0
  - org : null
---====================================
+--==============u7fnxr6d==============
 #cloud-boothook
 system
 hostname Router
@@ -30,7 +30,7 @@ aaa authentication login default local
 aaa authorization console
 aaa authorization exec default local
 login on-success log
---====================================--
+--==============u7fnxr6d==============--
 EOF
 
 # Generate iso file containing config file 
